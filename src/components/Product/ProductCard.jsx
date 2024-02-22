7e6f91d1d6d6dfa238a5e34c9597aea438c8ed26
ByZoom69 <57224900+ByZoom69@users.noreply.github.com>
2024-02-22 12:29:33 +0100

    fix

--- src/components/Product/ProductCard.jsx
+++ src/components/Product/ProductCard.jsx
@@ -48,18 +48,32 @@ const ProductCard = (product) => {
         }}
       />
       <ul onMouseOver={() => setShowHideBtn(true)} onMouseLeave={() => setShowHideBtn(false)} className='transition duration-500 ease-in-out transform hover:scale-105 hover:shadow-lg border-2 rounded w-auto h-auto flex flex-col justify-end'>
-           <Link to={'/productdetails/' + product.product.id}><li><img src={ProductImg} alt="" className="" /></li></Link>
+           <Link to={'/productdetails/' + product.product.id}>
+              <li>
+                <img src={ProductImg} alt="" className="" />
+              </li>
+            </Link>
             { user.isAuth ?
                 <Link onClick={() => {addToCart(product.product.id)}} className={`${showHideBtn ? 'visible' : 'invisible'} text-white relative bgPrimaryColor p-2 h-10 text-center`}>AJOUTER AU PANIER</Link>
               :
                 <div></div>
             }
-          <li className='pl-2 pt-2'><h4 className="text-lg md:text-xl lg:text-2xl">{product.product.productName}</h4></li>
-          <li className='pl-2'><p className="text-base md:text-lg">{product.product.brand.name}</p></li>
-          <li className='pl-2 pb-2 flex'>
-            <span className="mr-2 text-lg font-semibold">{product.product.price} €</span>
-            <span className="text-base md:text-lg line-through text-[#951D46] bg-[#FFB4CD] rounded-full pr-1 pl-1">200 €</span>
-          </li>
+          <Link to={'/productdetails/' + product.product.id}>
+            <li className='pl-2 pt-2'>
+              <h4 className="text-lg md:text-xl lg:text-2xl">{product.product.productName}</h4>
+            </li>
+          </Link>
+          <Link to={'/productdetails/' + product.product.id}>
+            <li className='pl-2'>
+              <p className="text-base md:text-lg">{product.product.brand.name}</p>
+            </li>
+          </Link>
+          <Link to={'/productdetails/' + product.product.id}>
+            <li className='pl-2 pb-2 flex'>
+              <span className="mr-2 text-lg font-semibold">{product.product.price} €</span>
+              <span className="text-base md:text-lg line-through text-[#951D46] bg-[#FFB4CD] rounded-full pr-1 pl-1">200 €</span>
+            </li>
+          </Link>
       </ul>
     </>
   );
